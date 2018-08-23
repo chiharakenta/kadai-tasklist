6ca81e6829558a60054281528624943764f35fc6
kenta.chihara <chiharakenta0519@gmail.com>
2018-08-23 13:26:23 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+
+  
+  private
+  
+  def current_user
+    @current_user ||= User.find_by(id: [session[:user_id]])
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180819012424) do
+ActiveRecord::Schema.define(version: 20180822091330) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20180819012424) do
     t.string   "status"
     t.integer  "deadlinemonth"
     t.integer  "deadlinedate"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
