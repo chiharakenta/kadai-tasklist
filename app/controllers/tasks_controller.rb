462a57c560df2a1d256a0ad17e4fc737788cec84
kenta.chihara <chiharakenta0519@gmail.com>
2018-08-24 08:55:17 +0000

    link tasks to user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in, only: [:show, :new, :edit]
+  before_action :require_user_logged_in, only: [:show, :new, :create, :edit, :update, :destroy]
   
   def index
-    @task = Task.order(created_at: :desc).page(params[:page]).per(25)
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(25)
+    end
   end
   
   def show
@@ -14,7 +18,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に作成されました'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
-  validates :status, presence: true, length: {maximum: 255 }
+  validates :status, presence: true, length: {maximum: 3 }
   validates :deadlinemonth, presence: true, length: {maximum: 2 }
   validates :deadlinedate, presence: true, length: {maximum: 2 }
 end
